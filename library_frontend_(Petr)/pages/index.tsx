372ebb756080de0c56eaa21a75ec3dd6b3e94b62
CQuann <p9083718@gmail.com>
2022-12-20 22:41:28 +0300

    как-то так

--- library_frontend_(Petr)/pages/index.tsx
+++ library_frontend_(Petr)/pages/index.tsx
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
+import { withLayout } from '../Layout/Layout';
+import { Group } from '../components';
+import axios from "axios";
 
-export default function Home() {
+
+function Home() {
   return (
     <div>
-
+      <Group size='m'> 10 </Group>
+      <Group size='s' href='#'> 10-3 </Group>
     </div>
   )
 }
+
+export default withLayout(Home)
