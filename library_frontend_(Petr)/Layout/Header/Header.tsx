a977ce259472a4de91b5f35428c04f91aa52f754
CQuann <p9083718@gmail.com>
2023-01-14 22:11:56 +0300

    chlen

--- library_frontend_(Petr)/Layout/Header/Header.tsx
+++ library_frontend_(Petr)/Layout/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { HeaderProps } from './Header.props';
 import styles from './Header.module.css';
 import cn from 'classnames';
-import logo from './MainLogo.png';
+import mainLogo from './MainLogo.png';
 
 
 
@@ -9,7 +9,7 @@ export const Header = ({ ...props }: HeaderProps) => {
 	return (
 		<header {...props} className={cn(styles.header)}>
 			<div className={styles.icon}>
-				<img src={logo.src} alt="logo" className={styles.logo} />
+				<img src={'./MainLogo.png'} alt="logo" className={styles.logo} />
 			</div>
 			<div className={styles.name}>
 				Library
