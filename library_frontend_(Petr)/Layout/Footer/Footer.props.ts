c87d10244062623c1fa573a1eb034c1fa8ef2578
CQuann <p9083718@gmail.com>
2023-02-06 02:13:03 +0300

    удаляю остатки старого фронта

--- library_frontend_(Petr)/Layout/Footer/Footer.props.ts
+++
@@ -1,3 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface FooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> { }
--- library_frontend_(Petr)/Layout/Header/Header.props.ts
+++
@@ -1,3 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface HeaderProps extends DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> { }
